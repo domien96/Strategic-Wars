92661eb11e0b0a45e843302a03333513168a2f89
Jorgin Herremans <jorgin.herremans@ugent.be>
2015-10-10 10:02:15 +0200

    Implementation common.c

--- LevelEditor/common.c
+++ LevelEditor/common.c
@@ -10,25 +10,26 @@ Pos pos_init(int row, int col)
 
 Path* path_alloc(int step_count, int distance)
 {
-    //TODO
-    return NULL;
+	Path* path = (Path*)calloc(step_count,sizeof(Pos));
+	(*path).distance = distance;
+	(*path).step_count = step_count;
+    return path;
 }
 
 void path_free(Path* path)
 {
-    //TODO
+	free(path->steps);
+	free(path);
 }
 
 
 int cell_type_is_unit(CellType cell_type)
 {
-    //TODO
-    return 0;
+    return cell_type == UNIT_1 || cell_type == UNIT_2 || cell_type == UNIT_3;
 }
 
 int cell_type_is_player_owned(CellType cell_type)
 {
-    //TODO
-    return 0;
+	return cell_type_is_unit(cell_type) || cell_type == HEADQUARTER;
 }
 
