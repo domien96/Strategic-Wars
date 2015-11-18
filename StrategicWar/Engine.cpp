7e3908974223bdb92021183d9899150be9b2b1c2
shendrik <silkehendrikx@hotmail.com>
2015-11-18 13:39:52 +0100

    engine implementatation(systems methods)

--- StrategicWar/Engine.cpp
+++ StrategicWar/Engine.cpp
@@ -22,10 +22,15 @@ void Engine::UpdateEntity(Entity* entity)
 	EntityChanged(entity);
 }
 
+/* If the System is not yet attached to the Engine, then it will be added. We will also set the engine pointer of the System to this Engine. */
 void Engine::AddSystem(System* system)
 {
-	// TODO: Add System to engine
-	// TODO: Set engine pointer of system to this engine
+	std::vector<System*>::iterator it;
+	it = find(systems.begin(), systems.end(), system);
+	if (it == systems.end()) {
+		systems.push_back(system);
+	}
+	system->SetEngine(this);
 }
 
 /* In case the given Entity is attached to the Engine, it is removed and the EntityStream is warned. */
@@ -39,10 +44,15 @@ void Engine::RemoveEntity(Entity* entity)
 	}
 }
 
+/* In case the given System is attached to the Engine, it is removed. We will also reset the engine pointer of the System to NULL. */
 void Engine::RemoveSystem(System* system)
 {
-	// TODO: Remove System from engine
-	// TODO: Reset engine pointer of system to NULL
+	std::vector<System*>::iterator it;
+	it = find(systems.begin(), systems.end(), system);
+	if (it != systems.end()) {
+		systems.erase(it);
+	}
+	system->SetEngine(NULL);
 }
 
 /* Returns all Entities currently attached to the Engine. */
@@ -51,11 +61,15 @@ vector<Entity*> Engine::GetEntities()
 	return entities;
 }
 
+
+/* Calls Updates on all added Systems in order */
 void Engine::Update()
 {
-	// TODO: Call Update() on all assigned systems in a fixed order.
+	std::vector<System*>::iterator it;
+	for (it = systems.begin(); it < systems.end(); ++it) {
+		(*it)->Update();
+	}
 }
-
 
 void Engine::EntityAdded(Entity* entity)
 {
