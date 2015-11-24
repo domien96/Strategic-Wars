1eef6d01f2d37f72ec6bcb2798fe5405584a5fe3
domien van steendam <domien.vansteendam@ugent.Be>
2015-11-25 00:25:54 +0100

    Added testproject

---
+++ StrategicWarTest/test_strategicWar.cpp
@@ -0,0 +1,19 @@
+int main() {
+	runTests();
+}
+
+void runTests() {
+	// DOE TOPDOWN TESTEN, WANT HET BESPAART VEEL WERK.
+	// Voeg methodes toe en schrijf daarin je testen
+	sampleTest();
+
+	// Testen voor entity
+
+	// Testen voor ?
+	// Testen voor ?
+	// enz...
+}
+
+void sampleTest() {
+	//test something
+}
