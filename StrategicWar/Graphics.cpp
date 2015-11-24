9e836b91326f0ebecf8475d0cf78fb6b4d69ccde
jjocqu <jens.jocque@ugent.be>
2015-11-24 09:15:27 +0100

    implemented Graphics::LoadSpriteCache

--- StrategicWar/Graphics.cpp
+++ StrategicWar/Graphics.cpp
@@ -23,8 +23,70 @@ void Graphics::LoadFonts()
 void Graphics::LoadSpriteCache() {
 	sprites.resize(SPRITE_LENGTH);
 	sprites[SPRITE_NONE] = al_load_bitmap("../assets/images/dev/actor.png");
-	// TODO: initialize all sprites as a cache.
-	
+	sprites[SPRITE_ARCHERY] = al_load_bitmap("../assets/images/dev/archery.png");
+	sprites[SPRITE_BADGE_AP] = al_load_bitmap("../assets/images/dev/badge_ap.png");
+	sprites[SPRITE_BADGE_ATTACK] = al_load_bitmap("../assets/images/dev/badge_attack.png");
+	sprites[SPRITE_BADGE_HP] = al_load_bitmap("../assets/images/dev/badge_hp.png");
+	sprites[SPRITE_BADGE_RANGE_MAX] = al_load_bitmap("../assets/images/dev/badge_range_max.png");
+	sprites[SPRITE_BADGE_RANGE_MIN] = al_load_bitmap("../assets/images/dev/badge_range_min.png");
+	sprites[SPRITE_BRIDGE] = al_load_bitmap("../assets/images/dev/bridge.png");
+	sprites[SPRITE_BRIDGE2] = al_load_bitmap("../assets/images/dev/bridge2.png");
+	sprites[SPRITE_CROSS_GREY] = al_load_bitmap("../assets/images/dev/cross_grey.png");
+	sprites[SPRITE_CROSSHAIR] = al_load_bitmap("../assets/images/dev/crosshair.png");
+	sprites[SPRITE_DESTINATION] = al_load_bitmap("../assets/images/dev/destination.png");
+	sprites[SPRITE_FIRE] = al_load_bitmap("../assets/images/dev/fire.png");
+	sprites[SPRITE_FLAG1] = al_load_bitmap("../assets/images/dev/flag1.png");
+	sprites[SPRITE_FLAG2] = al_load_bitmap("../assets/images/dev/flag2.png");
+	sprites[SPRITE_HEALTH_HALF] = al_load_bitmap("../assets/images/dev/health0.5.png");
+	sprites[SPRITE_HEALTH_ONE] = al_load_bitmap("../assets/images/dev/health1.png");
+	sprites[SPRITE_HEALTH_TWO] = al_load_bitmap("../assets/images/dev/health2.png");
+	sprites[SPRITE_HEALTH_THREE] = al_load_bitmap("../assets/images/dev/health3.png");
+	sprites[SPRITE_HEALTH_FOUR] = al_load_bitmap("../assets/images/dev/health4.png");
+	sprites[SPRITE_HEALTH_FIVE] = al_load_bitmap("../assets/images/dev/health5.png");
+	sprites[SPRITE_HEALTH_SIX] = al_load_bitmap("../assets/images/dev/health6.png");
+	sprites[SPRITE_HEALTH_SEVEN] = al_load_bitmap("../assets/images/dev/health7.png");
+	sprites[SPRITE_HEALTH_EIGHT] = al_load_bitmap("../assets/images/dev/health8.png");
+	sprites[SPRITE_HEALTH_NINE] = al_load_bitmap("../assets/images/dev/health9.png");
+	sprites[SPRITE_HEALTH_TEN] = al_load_bitmap("../assets/images/dev/health10.png");
+	sprites[SPRITE_HQ] = al_load_bitmap("../assets/images/dev/hq.png");
+	sprites[SPRITE_HUD] = al_load_bitmap("../assets/images/dev/hud.png");
+	sprites[SPRITE_ICON] = al_load_bitmap("../assets/images/dev/icon.png");
+	sprites[SPRITE_INFANTRY] = al_load_bitmap("../assets/images/dev/infantry.png");
+	sprites[SPRITE_LMB] = al_load_bitmap("../assets/images/dev/lmb.png");
+	sprites[SPRITE_PATH] = al_load_bitmap("../assets/images/dev/path.png");
+	sprites[SPRITE_PATH_FAR] = al_load_bitmap("../assets/images/dev/path_far.png");
+	sprites[SPRITE_RMB] = al_load_bitmap("../assets/images/dev/rmb.png");
+	sprites[SPRITE_ROAD_0] = al_load_bitmap("../assets/images/dev/road_0.png");
+	sprites[SPRITE_ROAD_1] = al_load_bitmap("../assets/images/dev/road_1.png");
+	sprites[SPRITE_ROAD_2] = al_load_bitmap("../assets/images/dev/road_2.png");
+	sprites[SPRITE_ROAD_3] = al_load_bitmap("../assets/images/dev/road_3.png");
+	sprites[SPRITE_ROAD_4] = al_load_bitmap("../assets/images/dev/road_4.png");
+	sprites[SPRITE_ROAD_5] = al_load_bitmap("../assets/images/dev/road_5.png");
+	sprites[SPRITE_ROAD_6] = al_load_bitmap("../assets/images/dev/road_6.png");
+	sprites[SPRITE_ROAD_7] = al_load_bitmap("../assets/images/dev/road_7.png");
+	sprites[SPRITE_ROAD_8] = al_load_bitmap("../assets/images/dev/road_8.png");
+	sprites[SPRITE_ROAD_9] = al_load_bitmap("../assets/images/dev/road_9.png");
+	sprites[SPRITE_ROAD_10] = al_load_bitmap("../assets/images/dev/road_10.png");
+	sprites[SPRITE_ROAD_11] = al_load_bitmap("../assets/images/dev/road_11.png");
+	sprites[SPRITE_ROAD_12] = al_load_bitmap("../assets/images/dev/road_12.png");
+	sprites[SPRITE_ROAD_13] = al_load_bitmap("../assets/images/dev/road_13.png");
+	sprites[SPRITE_ROAD_14] = al_load_bitmap("../assets/images/dev/road_14.png");
+	sprites[SPRITE_ROAD_15] = al_load_bitmap("../assets/images/dev/road_15.png");
+	sprites[SPRITE_ROCK] = al_load_bitmap("../assets/images/dev/rock.png");
+	sprites[SPRITE_ROCK_2] = al_load_bitmap("../assets/images/dev/rock_2.png");
+	sprites[SPRITE_ROCK_3] = al_load_bitmap("../assets/images/dev/rock_3.png");
+	sprites[SPRITE_SELECT] = al_load_bitmap("../assets/images/dev/select.png");
+	sprites[SPRITE_SELECT_HIGHLIGHT] = al_load_bitmap("../assets/images/dev/select_highlight.png");
+	sprites[SPRITE_SHIELD] = al_load_bitmap("../assets/images/dev/shield.png");
+	sprites[SPRITE_TARGET] = al_load_bitmap("../assets/images/dev/target.png");
+	sprites[SPRITE_TARGET_BLAST] = al_load_bitmap("../assets/images/dev/target_blast.png");
+	sprites[SPRITE_TERRAIN] = al_load_bitmap("../assets/images/dev/terrain.png");
+	sprites[SPRITE_TERRAIN_2] = al_load_bitmap("../assets/images/dev/terrain_2.png");
+	sprites[SPRITE_UNIT_37] = al_load_bitmap("../assets/images/dev/unit_37.png");
+	sprites[SPRITE_UNIT_8] = al_load_bitmap("../assets/images/dev/unit_8.png");
+	sprites[SPRITE_WAIT] = al_load_bitmap("../assets/images/dev/wait.png");
+	sprites[SPRITE_WATER] = al_load_bitmap("../assets/images/dev/water.png");
+
 	// World: pointer set to NULL so it can be filled in later
 	sprites[SPRITE_WORLD] = NULL;
 }
