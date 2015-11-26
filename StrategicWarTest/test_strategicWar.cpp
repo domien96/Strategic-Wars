a6371316221f7ef90e797c6ebeaea96299c15a0b
Jorgin Herremans <jorgin.herremans@ugent.be>
2015-11-26 18:05:13 +0100

    add test_engine_add_entry

--- StrategicWarTest/test_strategicWar.cpp
+++ StrategicWarTest/test_strategicWar.cpp
@@ -1,19 +1,35 @@
-int main() {
-	runTests();
+#include "../StrategicWar/Engine.h"
+#include "../StrategicWar/Entity.h"
+#include "../StrategicWar/EntityStream.h"
+#include "../StrategicWar/Game.h"
+#include "../StrategicWar/Pathfinder.h"
+#include <assert.h>
+
+
+void test_engine_add_entity() {
+	Vector2 v;
+	Engine e(new Context(v));
+	e.AddEntity(new Entity());
+	assert(e.GetEntities().size()==1);
 }
+
 
 void runTests() {
 	// DOE TOPDOWN TESTEN, WANT HET BESPAART VEEL WERK.
 	// Voeg methodes toe en schrijf daarin je testen
-	sampleTest();
 
 	// Testen voor entity
+
+	// Testen voor Engine
+
+
 
 	// Testen voor ?
 	// Testen voor ?
 	// enz...
 }
 
-void sampleTest() {
-	//test something
+
+int main() {
+	runTests();
 }
