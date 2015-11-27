ec35d0e431870f07872a5c9628bf0e585b303775
dvsteend PC KLAS <domien.Vansteendam@ugent.be>
2015-11-27 14:10:18 +0100

    Added methods World.H

--- StrategicWar/World.h
+++ StrategicWar/World.h
@@ -1,6 +1,20 @@
 #ifndef WORLD_H
 #define WORLD_H
 
-class World { public: int rows, columns; };
+class World { 
+	public: 
+		int getRows();
+
+		int getColumns();
+
+		// geef cel terug getCell(int row, int col);
+
+		void loadLevel(string world_file);
+
+		//bool level_can_walk_over(cell1, cell2)
+		
+	private: 
+		int rows, columns; 
+	};
 
 #endif
