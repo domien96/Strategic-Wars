c5bec0bdaf93f64a1c3c3e474f67bbc903285d3c
Jorgin Herremans <jorgin.herremans@ugent.be>
2015-12-05 14:00:57 +0100

    AnimationSystem Update()

--- StrategicWar/AnimationComponent.h
+++ StrategicWar/AnimationComponent.h
@@ -13,14 +13,18 @@ public:
 	/* New position, only if the animation is a movement */
 	Grid* new_pos;
 	
+	/* True if this unit is the attacker, false if it is being attacked; only if the animation is an attack */
+	bool is_attacker;
+
 	/* New hp, only if the animation is an attack*/
 	int new_hp;
 
 
 	/* Call if animation is a movement*/
-	AnimationComponent(Grid& _new_pos) : is_attack(false), new_pos(&_new_pos), new_hp(0) {};
+	AnimationComponent(Grid& _new_pos) : is_attack(false), new_pos(&_new_pos), is_attacker(false), new_hp(0) {};
 	/* Call if animation is an attack*/
-	AnimationComponent(int _new_hp) : is_attack(true), new_pos(NULL), new_hp(_new_hp) {};
+	AnimationComponent(int _new_hp, bool _is_attacker) :
+		is_attack(true), new_pos(NULL), is_attacker(_is_attacker), new_hp(_new_hp) {};
 
 	virtual ~AnimationComponent() {};
 	virtual Tag GetTag() { return ANIMATION; };
--- StrategicWar/AnimationSystem.h
+++ StrategicWar/AnimationSystem.h
@@ -2,6 +2,8 @@
 #define ANIMATION_SYSTEM_H
 
 #include "System.h"
+#include "AnimationComponent.h"
+#include "PositionComponent.h"
 
 class AnimationSystem : public System
 {
@@ -16,7 +18,24 @@ protected:
 		vector<Entity*> entities = engine->GetEntities();
 		Context* context = engine->GetContext();
 
-		set<Entity*> en = es->WithTag(Component::ANIMATION);
+		set<Entity*> entity_set = es->WithTag(Component::ANIMATION);
+		for (set<Entity*>::iterator i = entity_set.begin(); i!= entity_set.end(); i++) {
+			Entity* entity = *i;
+			AnimationComponent* ac = (AnimationComponent*) entity->GetComponent(Component::ANIMATION);
+			if (ac->is_attack) {
+				if (ac->is_attacker) {
+					// TODO
+				}
+				else {
+					UnitComponent* uc = (UnitComponent*) entity->GetComponent(Component::UNIT);
+					uc->hp = ac->new_hp;
+				}
+			}
+			else {
+				PositionComponent* pc = (PositionComponent*)entity->GetComponent(Component::POSITION);
+				pc->pos = ac->new_pos;
+			}
+		}
 		
 
 		// TODO 
