5cac2ad1e9818c28605eb6356aa1e667ebea8bfc
shendrik <silkehendrikx@hotmail.com>
2015-12-06 23:14:39 +0100

    comment removed

--- StrategicWar/MouseSystem.h
+++ StrategicWar/MouseSystem.h
@@ -237,85 +237,20 @@ protected:
 						break;
 					}
 				}
-				//cout << "Button down" << clickedGrid->col << "-" << clickedGrid->row << "!" << endl;
-
-				Entity* clickedEntity = world->getWorldEntity(clickedGrid->row, clickedGrid->col, 1);
-
-				/*if (clickedEntity != nullptr) {
-				UnitComponent *uc = static_cast<UnitComponent*>(clickedEntity->GetComponent(Component::UNIT));
-
-				if (uc != nullptr) {
-
-				//  Show yellow selection square at the correct square.
-				Entity* yellow_selector = new Entity();
-				yellow_selector->Add(new TextureComponent(Graphics::Sprite::SPRITE_SELECT_HIGHLIGHT));
-				yellow_selector->Add(new PositionComponent(*clickedGrid, 3));
-				world->GetWorldEntities(3)->push_back(yellow_selector);
-
-				int player = uc == NULL ? 0 : uc->player;
-				if (uc != NULL) {
-				UnitComponent::UnitType type = uc->type;
-
-				if ((player == 0) && (UnitComponent::UnitType::HQ != type)) {
-				selectedUnit = new Entity(*clickedEntity);
-				}
-				}
-
-				}
-
-				}*/
+				
 
 			}
 
+			/*
+			//  Show yellow selection square at the correct square.
+			Entity* yellow_selector = new Entity();
+			yellow_selector->Add(new TextureComponent(Graphics::Sprite::SPRITE_SELECT_HIGHLIGHT));
+			yellow_selector->Add(new PositionComponent(*clickedGrid, 3));
+			engine->AddEntity(yellow_selector);
+			*/
 
 		}
 
-		/*ALLEGRO_MOUSE_STATE state;
-		al_get_mouse_state(&state);
-
-		if (state.buttons && 1) { // linker // KLIK 1
-		Vector2 v = Vector2(state.x, state.y);
-		Grid* clickedGrid = &Graphics::Instance().ToGrid(v);
-		cout << "Button down" << clickedGrid->col << "-" << clickedGrid->row << "!" << endl;
-
-		World* world = engine->GetContext()->getworld();
-		Entity* clickedEntity = world->getWorldEntity(clickedGrid->row, clickedGrid->col, 1);
-
-
-
-		}
-		}
-		else /*if (selectedUnit != nullptr && selectedGrid->col >0)// != nullptr) {
-
-		// Er is wel een unit geselecteerd.
-
-		/*cout << "yes" << endl;
-
-		Pathfinder finder;
-		Path* p = finder.find_path(*engine->GetContext()->getworld(),
-		*(UnitComponent*) selectedUnit->GetComponent(Component::UNIT),
-		*selectedGrid, *&Graphics::Instance().ToGrid(Vector2(state.x, state.y)));
-		finder.reset();*/
-		//delete finder;
-
-		//TODO: find_path moet opgeroepen worden zelfs als er nog geen bevestigende klik is geweest.
-
-		//int path_length = p->cost;
-		//Grid* steps = p->steps;
-
-		//Zet mag enkel toegelaten worden als path_length <= range vd unit
-
-
-		// Kan je er naartoe verplaatsen?
-		// doe dit
-		// methode ergens aanroepen of via Path* path = Pathfinder::find_path(world, selectedUnitComponent, selectedGrid, clickedPosition);
-		// teken voetjes op alle Grid's die path teruggeeft
-		// Is het een vijand?
-		// val aan
-		// Is het een ander mannetje van jezelf
-		// zie dit als een nieuwe klik en bekijk de vorige if constructie
-
-		// un-select na verplaatsing en aanval
 	}
 
 	virtual Type GetType() { return System::TYPE_MOUSE; };
